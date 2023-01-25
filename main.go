cae3a2c05f175b873a2ccd208db31edc4dcca2b1
Khalid Al-Mazrouai <khalid.almazrouai@gmail.com>
2023-01-25 19:44:25 +0400

    not rihal team solution

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
+import (
+	"strconv"
+)
+
 // Solve returns true if the number is a K number
 func Solve(number int) bool {
-	// Write your code here
+	n := number * number
+	if n < 10 {
+		return true
+	}
+
+	s := strconv.Itoa(n)
+	for i := 1; i < len(s); i++ {
+		x, _ := strconv.Atoi(s[0:i])
+		y, _ := strconv.Atoi(s[i:])
+
+		if x > number {
+			return false
+		}
+
+		if y == 0 || y > number {
+			continue
+		}
+
+		if x+y == number {
+			return true
+		}
+	}
 
 	return false
 }
